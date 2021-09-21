92b30ed8d3af0e94c5b712018c72ed8867d66855
Kelrugem <50429899+Kelrugem@users.noreply.github.com>
2021-09-21 21:38:17 +0200

    Add files via upload

---
+++ scripts/data_common2.lua
@@ -0,0 +1,6 @@
+function onInit()
+	table.insert(DataCommon.dmgtypes, "ballistic");
+	table.insert(DataCommon.dmgtypes, "laser");
+	table.insert(DataCommon.dmgtypes, "electric");
+	table.insert(DataCommon.dmgtypes, "radiation");
+end
